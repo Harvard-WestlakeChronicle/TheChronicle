5d2009ab395ca4faddc60599d769d87b40c11840
David Woldenberg <dwoldenberg1@gmail.com>
2014-10-12 22:20:45 -0700

    Added Fair Game spspecial feature, testing this method of implementation out. Thanks t razorfish on github for his parallax.js library.

---
+++ features_/fair_game/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<title>Fair Game</title>
+
+	<link href="css/reset.css"  rel="stylesheet" type="text/css" />
+	<link href="css/chrome.css" rel="stylesheet" type="text/css" />
+	<link href="css/style.css"     rel="stylesheet" type="text/css" />
+
+	<!--[if lt IE 9]>
+		<script src="js/html5.js"></script>
+	<![endif]-->
+
+	<script type="text/javascript" src="js/jquery-1.7.1.js"></script>
+	<script type="text/javascript" src="js/side-nav.js"></script>
+	<script type="text/javascript" src="js/backgrounds.js"></script>
+	<script type="text/javascript" src="js/kinetics.js"></script>
+
+	<!-- parallax.js really screws with the DOM, so make sure it's included last so other things don't notice :) -->
+	<!-- https://github.com/razorfish/Parallax-JS -->
+	<script type="text/javascript" src="js/parallax.js"           ></script>
+	<script>
+		$(document).ready(function(){
+			/*var urls = ["js/jquery-1.7.1.js", "js/side-nav.js", "js/backgrounds.js", "js/kinetics.js"];
+			for (i =0; i < urls.length; ++i){
+				var s = document.createElement("script");
+				s.type = "text/javascript";
+				s.src = urls[];
+				$("head").append(s);
+
+			}*/
+			$('img[src="images/fair_game_2.jpg"]').css("width", "100%");
+		});
+	</script>
+
+</head>
+<body>
+	<!-- this is to make the page's vertical scrollbar appear the correct height -->
+	<div id="mock-scroller"></div>
+
+	<a href="/" class="go-back" style="display: inline;">Back to HWchronicle</a>
+	<div id="content">	
+		<section id="story-header" class="photo story-header">
+			<img class="bg animate blur" width="100%" src="images/fair_game_2.jpg" anim-detached="true" />
+			<div class="story">
+				<div id="header-main">
+					<h1>Fair Game</h1>
+					<p style="line-height: 30px;"><span class="author">By Zoe Dutton</span></p>
+					<p>While female athletic teams prove their prowess on the court, field and in the pool,<br>the majority of students believe they are treated differently than their male counterparts.</p>
+					<p>Scroll down to contine reading</p>
+					<img src="images/infographic-2.png" />
+				</div>
+			</div>
+		<span class="designer">Design by David Woldenberg</span>
+		</section>
+
+		<section id="story-intel" class="story-intel" style="z-index: -1;">
+			<div class="story btext">
+				<p class="first"><span id="drop">W</span>hen Sandra* ’15 slides on her kneepads and strides onto the volleyball court, she isn’t there to play nice.  As a member of a volleyball team that was CIF runner-up last year, she and her teammates are among the top players in Southern California. Their games are also among the school’s most popular, drawing crowds of hundreds of chanting, face-painted fans.</p>
+				<p>how does she feel when students casually reference the appeal of players’ teensy spandex shorts? Sandra says it doesn’t bother her.</p>
+				<p>“That’s just a joke,” she said. “I think because the volleyball girls are comfortable with it, it’s not really disrespectful.”</p>
+				<p>Sandra’s statement is part of a larger debate over the treatment of female athletes at Harvard-Westlake.</p>
+				<p>According to a Chronicle poll of 421 students, 65 percent believe that female athletes are treated differently from male athletes by students, while 45 percent think they are treated differently by the administration.</p>
+				<p>The issue has been an especially hot topic in recent weeks, after an assistant football coach made an offhand comment in which he compared his players’ poor tackling ability to that of the girls’ field hockey team while they were within earshot. The football coaches and Head of Athletics Terry Barnum later apologized, and Barnum also spoke to both about the importance of respecting female athletes.</p> 
+			</div>
+		</section>
+
+
+		<section id="story-drop" class="story-drop">
+			<div class="story wtext">
+				<blockquote class="blockq">
+					<p>“My message to them was… all athletes male and female are to be respected and to be treated fairly, no matter what sport they play and no matter what gender they are,” Barnum said.</p>
+				</blockquote>
+			</div>
+		</section>
+
+
+		<section id="story-intel" class="story-intel">
+			<div class="story btext">
+				<div>
+					<p>Yet some students say the problem is not a single coach’s remark, but the larger attitudes they believe it reflects.</p>
+					<p>“There’s definitely more of an appreciation and respect for the guys’ sports,” varsity track and field athlete Daisy ’15 said.</p>
+					<p>Barnum said the Athletics Department was looking into ways to encourage education among students about the importance of female athletics, possibly through a video or discussion at a First and Third Wednesday assembly. He did not, however, think that there was any sort of discrimination.</p>
+					<p>“The girls on the field hockey team felt like female athletes are treated by their peers or viewed by their peers as worse than male athletes. I'm not sure if I completely agree with that,” Barnum said. “I think in some instances that might be true. I'm not saying it's patently false. At the same time I think there are instances- our girls volleyball team, our girls soccer team, our girls track and field team- those are three instances, of girls that are held in very high regard by their peers, male and female alike, and feel very respected for the work they do and the work they've accomplished.”</p>
+					<p>Several members of the girls’ varsity soccer team, including Milly* ’15 agreed they had been treated with nothing but respect by students and administrators.</p>
+				</div>
+			</div>
+		</section>
+		
+		<section id="story-drop" class="story-drop">
+			<div class="story wtext">
+				<blockquote class="blockq">
+					<p>“Though I’ve never experienced anything bad, it could be different across different sports, depending on the culture around the team,” Milly said. “Field hockey, for example, doesn’t have a boys’ equivalent, and there’s definitely a negative, girly stigma around it.”</p>
+				</blockquote>
+			</div>
+		</section>
+		
+		<section id="story-intel" class="story-intel">
+			<div class="story btext">
+				<div>	
+					<p>However, the varsity girls’ soccer team ran into trouble last year over its methods of promotion.  A player urged students to attend a girls’ soccer game in a Facebook post that included the term “soccer sluts,” in what teammates say was a tongue-in-cheek parody of similarly offensive online party invitations.</p>
+					<p>Some students who viewed the post, however, didn’t consider it humorous.</p>
+					<p>“There were posts before that of a similar subject matter, along the lines of ‘come to our game because we’re hot,’ but I felt like this one was really crossing a line,” Ethan Weinstein ’15, who commented on the post, said. “They didn’t even mention that the girls’ soccer team was the number one team in the nation. It just seemed like their entire existence as a Harvard-Westlake identity was contingent upon the fact that they were hot girls wearing short shorts.”</p>
+					<p>Milly said she wasn’t offended by the post because it was meant as a joke, though she thought some of her teammates may have felt differently.</p>				
+				</div>
+			</div>
+		</section>
+
+		<section id="story-header" class="photo story-header">
+			<img class="bg animate" width="100%" min-width="100%" src="images/infographic.jpg" anim-detached="true" />
+		</section>
+		
+		<section id="story-intel" class="story-intel">
+			<div class="story btext">
+				<div>
+					<p>Soccer coach Richard Simms said such comments were absolutely unacceptable.</p>
+					<p>“It happens, and it's something I've been very serious about trying to discourage kids from doing,” Simms said. “I've been punitive about it in the past when I know our kids have done it and I've found out about it... I would say societal culture uses sex to sell things and promote things. At our school that's something we should discourage, and celebrate our girls as the athletes they are.”</p>
+					<p>While Sandra felt that comments like those made by the assistant football coach were inappropriate, she didn’t mind remarks about revealing spandex.</p>
+					<p>“I’m confident enough in myself and my own [athleticism] that it doesn’t bother me,” she said. “And if more people come [to games], great…  Some people can just get too politically correct.”</p>
+					<p>The athletic department discourages students from promoting games through objectification.</p>
+					<p>“It’s unfortunate,” Athletic Director Darlene Bible said. “That's trying to find ways to get people to come to their contests in the wrong manner.”</p>
+					<p>Student Athletic Advisory Committee member Jeremy Tepper ’15 didn’t believe such remarks were indicative of a larger issue.</p>
+					<p>“I would say that comments on Facebook groups by a specific person or any one person’s comments in general don’t reflect the student body and school as a whole,” said Tepper, who plays varsity football and volleyball.</p>
+					<p>Head of School Jeanne Huybrechts agreed, adding that remarks like the assistant football coach’s were rare.</p>
+					<p>“I could probably count on one hand the number of times that there has been friction between any two teams at this school, and by that I mean girls and boys or boys and boys,” Huybrechts said. “It just doesn’t happen very often.”</p>
+					<p>Field hockey player Jessica Spitz ’15 disagrees, saying that comments like the football coach’s should not be ignored.</p>
+				</div>
+			</div>
+		</section>
+		
+		<section id="story-drop" class="story-drop">
+			<div class="story wtext">
+				<blockquote class="blockq">
+					<p>“It's a lot easier to categorize this as an isolated incident rather than face the problem head on,” Spitz said. “The fact that this kind of behavior goes on at all is cause for concern, and writing it off as not reflective of the school's athletic culture is a mistake.”</p>
+				</blockquote>
+			</div>
+		</section>
+		
+		<section id="story-intel" class="story-intel end">
+			<div class="story btext">
+				<div>
+					<p>Spitz also wrote an opinion article criticizing the remark and the ethos she felt it reflected. The piece appeared in the fall issue of the school sports magazine Big Red and the Chronicle Online, and was widely circulated on social media, receiving over 3,500 page views.</p>
+					<p>Some, however, felt that the article blew the issue out of proportion.</p>
+					<p>“It wasn't even the football team saying it... it was one coach,” varsity football player Alec Dominick ’15 said. “I obviously think it's wrong, but it's not like he was even saying it to their face...obviously since the field hockey team doesn't play football they would tackle worse than us.”</p>
+					<p>Other students say that the issue is not so much the comments about female athletes as it is the turnout at their games.</p>
+					<p>Although volleyball is widely attended by student fans, many other girls’ sports are not.</p>
+					<p>“Let’s be honest- would you rather watch girls basketball or boys?” Franklin* ’16 asked. “Obviously boys. The game’s just more exciting, and girls can’t dunk like in boys’ basketball.”</p>
+					<p>Varsity track athlete Daisy says she tries not to let any gender inequity distract her from performing her best.</p>
+					<p>“Guys sports have traditionally been more valued everywhere,” Daisy said. “Harvard-Westlake is just a little part of that. It’s getting better, and we’re probably a lot better than most high schools, but it’s still an issue.”</p>
+					<p>*Names have been changed</p>
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<!-- Asynchronous google analytics; this is the official snippet.
+	 Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
+	 -->
+<script>
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35308943-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+	  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+	  ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+	  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+</script>
+</body>
+</html>
