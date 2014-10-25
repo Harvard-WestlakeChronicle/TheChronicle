434382d6ed0dc02e973116a8b9bade4762d0eaaf
David Woldenberg <dwoldenberg1@gmail.com>
2014-10-25 00:00:03 -0700

    update on Sat Oct 25 00:00:01 PDT 2014

--- wp-content/themes/Reset/search.php
+++ wp-content/themes/Reset/search.php
@@ -16,13 +16,120 @@
 	<?php /* Search Count */ 
 	$allsearch = &new WP_Query("s=$s&showposts=-1"); 
 	$key = wp_specialchars($s, 1); 
-	$count = $allsearch->post_count; _e(''); 
+	$count = $allsearch->post_count; _e('');
+	
+	add_filter('user_search_columns', 'user_search_columns_bd' , 10, 3);
+ 
+	function user_search_columns_bd($search_columns, $search, $this){
+		//if(!in_array($search_columns, 'display_name')){
+		if(!in_array('display_name', $search_columns)){
+			$search_columns[] = 'display_name';
+		}
+		return $search_columns;
+	}
+	//You are a goddess Sabuj: http://manchumahara.com/2014/04/03/search-user-by-display-name-in-wordpress-sitewide/
+	
+	// Here is where the magic happens
+	add_filter( 'get_meta_sql', 'user_meta_filter', 10, 6 );
 
+	function user_meta_filter( $sql, $queries, $type, $primary_table, $primary_id_column, $context ){
+	    // If it's not user forget it!
+	    if ( $type !== 'user' ){
+	        return $sql;
+	    }
+
+	    // Only if our variable is true then we will do the change
+	    if ( ! isset( $context->query_vars['meta_query']['replace_and'] ) || $context->query_vars['meta_query']['replace_and'] !== true ){
+	        return $sql;
+	    }
+
+	    $sql['where'] = preg_replace('/AND/', 'OR', $sql['where'], 1);
+	    return $sql;
+	}	
+
+
+	$args = array(
+	'search'         => $s,
+	'search_columns' => array( 'user_login', 'user_email'),
+	'meta_query' => array(
+		'relation' => 'OR',
+		'replace_and' => true, // Flag for the dark magic
+		0 => array(
+			'key'     => 'first_name',
+			'value'   => $s,
+			'compare' => 'LIKE'
+		),
+		1 => array(
+			'key'     => 'last_name',
+			'value'   => $s,
+			'compare' => 'LIKE'
+		)
+	)
+	);
+	
+	// The Query
+	$user_query = new WP_User_Query( $args ); 
+	
+	$count+= count($user_query);
+	
 	wp_reset_query(); 
 	?>
 	<h2 class="results-header sans-serif2 font18">Showing <?php echo $count; ?> results for <?php echo "'".$key."'"; ?>
 	
 	</h2>
+		<?php if ( ! empty( $user_query->results ) ) :
+			foreach ( $user_query->results as $user ) : ?>
+				
+			<div id="AuthorCenter" 
+			<?php
+			$key = 'currentSection';
+			$section = strtolower(get_user_meta($user->ID, $key, TRUE));
+			if($section == 'opinion'){
+		    	?>class="inner pop opinion"><?php
+			}elseif($section == 'ae'){
+		    	?>class="inner pop ae"><?php
+			}elseif($section == 'features'){
+		    	?>class="inner pop features"><?php
+			}elseif($section == 'sports'){
+		    	?>class="inner pop sports"><?php
+			}elseif($section == 'news'){
+		    	?>class="inner pop news"><?php
+			}else{
+		    	?>class="inner pop allChron"> <div class="wrapper"><?php } ?>
+			    	<?php
+			    		if(userphoto_exists($user->ID)){
+				    		echo '<div class="author-thumb">';
+					    	userphoto($user->ID); 
+			    		echo '</div>';
+		    		}
+
+	    	?>
+	    	<div class="author-info <?php if(userphoto_exists($user->ID)){echo "has-thumbnail"; } ?>">
+	    	      <h3 class="directory"><a href="<?php echo site_url(); ?>/about#staff">View staff directory >></a></h3>
+		   	    <h2><a href="<?php echo site_url(); ?>/staff/<?php echo $user->user_login ?>/"><?php echo $user->first_name.' '.$user->last_name; ?></a></h2>
+	
+			    <div class="sans-serif1 bold font14"><?php $key='currentPosition'; echo get_user_meta($user->ID, $key, TRUE); ?></div>
+		    </div>
+		    <div class="clear"></div>
+		    <div class="author-description sans-serif1 font13">
+		    	<?php 
+		    	$key = 'description';
+		    	echo get_user_meta($user->ID, $key, TRUE);
+		    	 ?>
+		    </div>
+		    <?php if($section == null){ ?>
+		      </div>
+		      <div class="colored clear">
+        		<div class="one"></div>
+        		<div class="two"></div>
+        		<div class="three"></div>
+        		<div class="four"></div>
+    		</div>
+    		<?php } ?>
+		</div>
+		<hr class="nonflair" />
+		 <?php endforeach; endif; ?>
+		 
 	<?php if (have_posts()) : ?>
 
 		<?php while (have_posts()) : the_post(); $wp_query->in_the_loop = false;
