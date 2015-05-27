58385021c5d630bfa51d05b98b239e75354a3f58
David Woldenberg <dwoldenberg1@gmail.com>
2015-05-27 00:00:16 -0700

    update on Wed May 27 00:00:01 PDT 2015

--- wp-content/themes/Reset/page-senior-section.php
+++ wp-content/themes/Reset/page-senior-section.php
@@ -62,65 +62,91 @@
 			</div>
 			
 			<div class="carousel">
-				<div class="full" >test</div>
-				<div class="full" >test</div>
-				<div class="full" >test</div>
+				<img id="banner" src="<?php bloginfo('template_directory') ?>/feature/senior_section/images/netflix2.png">
 			</div>
 			
 		</div>
 		<div class="years">
-			<div id="seven">
-				<div class="thumb">
+			<div>
+				<div id="seven" class="year first" >
+					<div class="title">
+						Seven
+					</div>
+					<div class="thumb">
+						<img height="320" width="240" src="<?php bloginfo('template_directory') ?>/feature/senior_section/images/freaksngeeks.jpg">
+					</div>
+					<div class="text">
+						The stamps on our virtual passports sealed the deal. We were no longer elementary school kids waiting anxiously to hear from secondary schools — we were Harvard-Westlake students. The rest of sixth grade passed in a blur of graduations and nervously interacting with our future classmates at that incredibly awkward pool party. In the fall, the transition to the North Faring campus proved somewhat difficult for most. We all felt like freaks, geeks, outsiders and newbies, and these feelings were only intensified by the much older and wiser eighth graders, who shouted "sevvies" at us as we ran to our classes (and attempted to navigate the convoluted maze that was Reynolds Hall). We were all trying to fit in, whether that meant finding a table on the terrace or sitting next to a potential friend in pre-algebra. Much like Sam Weir, we were new to the scene with millions of questions: what am I supposed to do with a free period? Do I actually have to go on retreat while it’s pouring rain? Am I supposed to grind at Young Angels/Bar and Bat Mitzvahs? Do I have to wear that embarrassing neon yellow vest for during-school SQUID and after-school SQUID? Is that sushi in the cafeteria? Our futures at Harvard-Westlake completely depended on whom we sat next to on the bus or partnered up with in P.E., or at least that’s how it seemed.
+					</div>
 				</div>
-				<div class="text">
-					seven
+				<div id="eight" class="year" >
+					<div class="title">
+						Eight
+					</div>
+					<div class="thumb">
+						<img height="320" width="240" src="<?php bloginfo('template_directory') ?>/feature/senior_section/images/gossipgirl.jpg">
+					</div>
+					<div class="text">
+						Blackberries seemed permanently attached to the hands of us eighth graders, who were slightly more comfortable in our environment but increasingly uncomfortable in our own skins. BBM was our form of a Gossip Girl website, and everyone knew about the latest breakups and make-ups. Some people seemed to have it all without even trying — the Serena Van Der Woodsens and the Nate Archibalds ruled the school, and everyone wanted to be them or be with them. The Blair Waldorfs and Chuck Basses of the grade knew everything about everyone and did not hesitate to spread rumors. And there were still the Lonely Boys and Little J’s, not yet completely used to the occasionally hostile surroundings of Middle School. Instead of hanging out on the Met steps, we hung out in Reynolds, where we were at risk of getting hit with a flying Gatorade bottle cap that was used in a game no one completely understood. Rather than vacationing in the Hamptons, we traveled to Joshua Tree or El Capitan. Harvard-Westlake fashion week compared to that of New York, featuring hair tinsel, feathers and Steve Madden combat boots. Most disappointingly, our dreams of a Winter Ball were crushed when Semiformal was canceled. Despite the seemingly endless drama, we were able to comfort ourselves with fresh cookies every day during seventh period.
+					</div>
+				</div>
+				<div id="nine" class="year" >
+					<div class="title">
+						Nine
+					</div>
+					<div class="thumb">
+						<img height="320" width="240" src="<?php bloginfo('template_directory') ?>/feature/senior_section/images/fridaynight.jpg">
+					</div>
+					<div class="text">
+						Finally — high school. Though we still remained physically at the Middle School, we felt like we were already on Coldwater. We joined JV and varsity teams, called ourselves Fanatics and showed up at Loyola games decked out in black and red. We attended "Friday Night Lights" football games and tried to acquaint ourselves with the campus we would call home next year. But we also had to deal with the not-so-fun parts of high school. Our grades actually mattered now, and we began to hear occasional mutterings of the "c" word — college. However, we still reveled in middle school traditions: ninth grade retreat, during which we suffered through days of not showering and donkey poop on the camp grounds; the dodgeball tournament run by our own Coach Taylor, Paul Mastin, and won by our own Dillon Panthers, the undefeatable AH?!; and (something that still pains a few of us to this day) copies of the Spectrum strewn across the ninth grade locker area as we tried to force our classmates to read them. We ended our freshman year on a high note at the Happiest Place on Earth, optimistically looking forward to the Upper School next year. We relaxed peacefully on the field, because as far as we were concerned, clear eyes, full hearts, can’t lose.
+					</div>
 				</div>
 			</div>
-			<div id="eight">
-				<div class="thumb">
+			<div>
+				<div id="ten" class="year first" >
+					<div class="title">
+						Ten
+					</div>
+					<div class="thumb">
+						<img height="320" width="240" src="<?php bloginfo('template_directory') ?>/feature/senior_section/images/oitnb.jpg">
+					</div>
+					<div class="text">
+						Oh, how wrong we were. Our transition turned out to be more of an incarceration. Like Piper, we were wide-eyed and frightened as we left our comfortable home at the Middle School and arrived, slowly, due to the traffic on Coldwater, at the Upper School, where in-class English essays were as frequent and unwelcome as strip searches, and history multiple choice was as difficult to swallow as prison food. We were forced to sign the Honor Code on every assignment, just as Piper was forced to sign away her freedom. We had gone from being the big fish at the Middle School to being the tiniest of minnows, getting pelted with water balloons on the quad by veterans who had been jailed for over two years. We were also new to the strict rules of attendance laid out by the Warden Gabe Preciado, and were scared away from parking on Halkirk by Officer Sanders Jackson. At the beginning of second semester, the failure of Whiteout to replace Semiformal resembled the depressing atmosphere of a makeshift prison party. But, like Piper, we were determined to appreciate the little things, like the Peer Support water fight at the end of the year and the famed Gustavo Dudamel visiting to conduct our orchestra. Next up was the big one – junior year.
+					</div>
 				</div>
-				<div class="text">
-					eight
+				<div id="eleven" class="year" >
+					<div class="title">
+						Eleven
+					</div>
+					<div class="thumb">
+						<img height="320" width="240" src="<?php bloginfo('template_directory') ?>/feature/senior_section/images/houseofcards.jpg">
+					</div>
+					<div class="text">
+						Junior year was a ruthless battlefield of competition and manipulation. Frank Underwood has nothing on juniors who would push their best friends in front of a train if it would help them get into their dream college. The presidential race was similar to the race to first period from upper St. Michaels, and we were often panting and exhausted just when we realized we would need to pull an all-nighter. The Homecoming Dance was our Inaugural Ball, and we began the marathon that was junior year. We were given new, elite technology in the electronic ID and scheduling apps, making it easier and more efficient to plan the merciless takedowns of our peers. We tested our knowledge (or so we were told) on the SATs and ACTs, but we also got to enjoy the fabulous production of "Hairspray." The newly inaugurated President Rick Commons attempted to build community through Civitalks, an initiative that gained very little support among his constituents. Towards the end of the year, we started setting our sights on new offices in higher places, narrowing our college lists and looking ahead to a summer of internships and college applications. Our rise to power was almost complete, and our House of Cards was stacked precariously.
+					</div>
 				</div>
-			</div>
-			<div id="nine">
-				<div class="thumb">
-				</div>
-				<div class="text">
-					nine
-				</div>
-			</div>
-			<div id="ten">
-				<div class="thumb">
-				</div>
-				<div class="text">
-					ten
-				</div>
-			</div>
-			<div id="eleven">
-				<div class="thumb">
-				</div>
-				<div class="text">
-					eleven
-				</div>
-			</div>
-			<div id="twelve">
-				<div class="thumb">
-				</div>
-				<div class="text">
-					twelve
+				<div id="twelve" class="year" >
+					<div class="title">
+						Twelve
+					</div>
+					<div class="thumb">
+						<img height="320" width="240" src="<?php bloginfo('template_directory') ?>/feature/senior_section/images/friends.jpg">
+					</div>
+					<div class="text">
+						The beginning of senior year was just as jarring as Rachel Green’s move to the big city — we were thrust into the college process, and first-quarter grades seemed to be the end-all-be-all of early decision. But soon enough, the first quarter nightmare was over and more of our classmates found out where they were going to college. When second semester arrived, we were finally ON A BREAK. We bonded as closely as roommates as we played music at the senior tables, and we delighted in food as much as Joey Tribiani as we took advantage of our senior privileges and had lunch off campus. The 1st and 3rd Wednesday assemblies strengthened the "diverse and inclusive community" noted in the new mission statement. We coupled up like Ross and Rachel, Chandler and Monica and Mike and Phoebe for Senior Prom, as we danced our hearts out at the Millennium Biltmore Hotel. Last week, Senior Transition Day ended with a final coffee house in the lounge, which, although not as comfortable as the Central Perk, was pretty nice. The final countdown and dash to the pool on the last day of school marked the end of our high school experience, until we graduate next week. So, The Chronicle would like to offer a "clap clap clap clap" to the senior class of 2015.
+					</div>
 				</div>
 			</div>
 		</div>
 		
 	</body>
 	<script>
-		$(document).ready(function(){
+		/*$(document).ready(function(){
 		  $('.carousel').slick({
 		    accesibility: true,
 		    autoplay: true,
 		    arrows: true
 		  });
-		});
+		});*/
 	</script>
 </html>
